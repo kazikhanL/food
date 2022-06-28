501b74d3b47f82b6fda43a4b544a361f7364dc45
Kazikhan Gereykhanov <kazikhan.lezg@gmail.com>
2022-06-28 16:38:34 +0300

    init project

---
+++ frontend/src/constants/index.ts
@@ -0,0 +1,6 @@
+export const PHONE: string = "+7 999 999-99-99";
+export const EMAIL: string = "info@domainname.ru";
+export const ADDRESS: string = "Москва, Егорьевский проезд, 2";
+
+export const BEGINNING_OF_WORK: string = "9:00";
+export const END_OF_WORK: string = "22:00";
---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const Home: NextPage = () => {
+    return (
+        <div className="container">
+            <h1>Организация питания на выезде</h1>
+        </div>
+    );
+};
+
+export default Home;
